2a8542d04978a40a7af0417e63d14e61bd510d7c
enridaga <enricodaga@gmail.com>
2016-02-09 11:30:50 +0000

    EventMedia-1

---
+++ ppr-evaluation/use-cases/EventMedia-1.policies.pl
@@ -0,0 +1,44 @@
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/duty http://creativecommons.org/ns#Attribution') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#Distribution') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#DerivativeWorks') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/copy') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/prohibition http://creativecommons.org/ns#CommercialUse') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/modify') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#LastFM','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/license') . 
+
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Flickr','http://www.w3.org/ns/odrl/2/prohibition http://creativecommons.org/ns#CommercialUse') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Flickr','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/sublicense') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Flickr','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/sell') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Flickr','http://www.w3.org/ns/odrl/2/duty http://www.w3.org/ns/odrl/2/attribute') . 
+
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/modify') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/transfer') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/rent') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/sublicense') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/reproduce') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/transmit') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/distribute') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/sell') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/display') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Foursquare','http://www.w3.org/ns/odrl/2/permission http://www.w3.org/ns/odrl/2/use') . 
+
+
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Eventful','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/license') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Eventful','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/transfer') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Eventful','http://www.w3.org/ns/odrl/2/prohibition http://creativecommons.org/ns#Distribution') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Eventful','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/lease') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Eventful','http://www.w3.org/ns/odrl/2/prohibition http://www.w3.org/ns/odrl/2/sell') . 
+
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/duty http://creativecommons.org/ns#Notice') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#Reproduction') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#DerivativeWorks') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/duty http://creativecommons.org/ns#Attribution') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/duty http://creativecommons.org/ns#ShareAlike') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#DBPedia','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#Distribution') . 
+
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Musicbrain','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#DerivativeWorks') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Musicbrain','http://www.w3.org/ns/odrl/2/permission http://purl.oclc.org/NET/ldr/ns#extraction') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Musicbrain','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#Reproduction') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Musicbrain','http://www.w3.org/ns/odrl/2/permission http://creativecommons.org/ns#Distribution') . 
+has_policy('http://purl.org/datanode/ex/0.2/EventMedia/1#Musicbrain','http://www.w3.org/ns/odrl/2/permission http://purl.oclc.org/NET/ldr/ns#reutilization') . 
+
